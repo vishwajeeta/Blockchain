0f19504c2bf2d3e24471d9e3bc17f1f65fb8daa3
vishwajeeta <gs699542@gmail.com>
2024-04-03 14:52:10 +0530

    directly send ether to the selected contract

---
+++ smart contract/send eth to owner/send.sol
@@ -0,0 +1,18 @@
+pragma solidity 0.5.1;
+
+contract MyContract {
+    mapping(address => uint256) public balances;
+    address payable wallet;
+
+    constructor (address payable _wallet) public {
+        wallet=_wallet;
+    }
+    function buyToken() payable {
+        balances[msg.sender] += 1;
+        wallet.transfer(msg.value);
+    }
+
+    function() external payable{
+        buyToken();
+    }
+}
