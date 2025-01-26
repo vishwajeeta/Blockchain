ff3cd7152d6b1ae326994b49e94ecf28513c93da
vishwajeeta <gs699542@gmail.com>
2025-01-26 19:46:19 +0530

    updated

--- UPGRADABLE SMART CONTRACTS/src/BoxV1.sol
+++ UPGRADABLE SMART CONTRACTS/src/BoxV1.sol
@@ -1,13 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-contract BoxV1 {
+import {UUPSUpgradeable} from "@OpenZeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+// this is used to do proxy upgradeable works
+contract BoxV1 is UUPSUpgradeable {
     uint256 internal number;
 
+    function setNumber(uint256 _number)external{}
     function getNumber() external view returns(uint256){
         return number;
     }
     function version()external view returns(uint256){
-        return 1;
+        return 2;
     }
+    //anybody can upgrade this
+    function _authorizeUpgrade(address newImplementation) internal override {        }
 }
--- UPGRADABLE SMART CONTRACTS/src/BoxV2.sol
+++ UPGRADABLE SMART CONTRACTS/src/BoxV2.sol
@@ -1,17 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {UUPSUpgradeable} from "@OpenZeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-contract BoxV2 is UUPSUpgradeable {
+contract BoxV2 {
     uint256 internal number;
 
-    function setNumber(uint256 _number)external{}
     function getNumber() external view returns(uint256){
         return number;
     }
     function version()external view returns(uint256){
-        return 2;
+        return 1;
     }
-    //anybody can upgrade this
-    function _authorizeUpgrade(address newImplementation) internal override {        }
 }
